48d1a5a1c09c5f24d01378a3be5c95053a5fa9be
Ever Lizarraga <rolank.utn@gmail.com>
2026-05-22 17:04:08 -0300

    mvnw wrapper:wrapper

---
+++ src/main/java/ar/edu/utn/ba/proyecto0/Main.java
@@ -0,0 +1,27 @@
+package ar.edu.utn.ba.proyecto0;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hola desde Países Local");
+        System.out.println("Hola, soy Ever Lizarraga y este es mi Proyecto 0");
+        System.out.println("Linea 1");
+        System.out.println("Linea 2");
+        System.out.println("Linea 3");
+        int paisesCargadors = 0;
+        System.out.println("Tengo " + paisesCargadors + " paises cargados.");
+
+        // eje 04
+        int a = 10;
+        Integer b = 3;
+        System.out.println("a + b = " + (a+b));
+        System.out.println("a + b = " + a+b);
+        System.out.println("a / b = " + (a/b));
+        System.out.println("a % b = " + (a%b));
+
+        // eje 05
+        int contador = 0;
+        contador += 1;
+        contador++;
+        System.out.println("Contador: " + contador);
+    }
+}
